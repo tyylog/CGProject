f81a1baa6776883c3e80aed568627b0bb4655d92
2022148042 <tyy@yonsei.ac.kr>
2025-11-25 03:04:23 +0900

    Disable debug mode before push

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // src/main.js
 import { Game } from './core/Game.js';
 
-const DEBUG_MODE = true;
+const DEBUG_MODE = false;
+
 if (DEBUG_MODE) {
     document.getElementById("start-screen")?.remove();
     document.getElementById("begin-screen")?.remove();
