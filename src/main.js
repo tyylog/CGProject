840f9ea44c7bb81f9011e68a95ee9a6e5ac1db0f
2022148042 <tyy@yonsei.ac.kr>
2025-12-03 19:17:22 +0900

    Debug mode false

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // src/main.js
 import { Game } from './core/Game.js';
 
-const DEBUG_MODE = true;
+const DEBUG_MODE = false;
 
 if (DEBUG_MODE) {
     document.getElementById("start-screen")?.remove();
